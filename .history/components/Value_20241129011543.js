49e8521da5f8b6ae427aa0e3722d9eac3bd82ce4
gavinPahal <gpahal4@my.bcit.ca>
2024-11-29 02:15:54 -0800

    Core values

--- .history/components/Value_20241129011543.js
+++ .history/components/Value_20241129011543.js
@@ -8,12 +8,12 @@ export default function Features() {
       </div>
 
       <div className="valueCards">
-        {/* Safety */}
         <div className="valueCard" style={{ margin: "0 10px" }}>
           <div className="textContent">
             <h3>Safety</h3>
             <p>
-              GoldiBite places significant emphasis on ensuring user safety by providing quick and reliable assistance in the event of food reactions. The platform is designed to empower users with the tools and resources they need to respond promptly and effectively to any unexpected situation. By prioritizing user well-being, GoldiBite ensures that individuals can handle emergencies with confidence and peace of mind.
+              GoldiBite places significant emphasis on ensuring user safety by providing quick and reliable assistance in the event of food reactions. The platform is designed to empower users with the tools and resources they need to respond promptly and effectively to any unexpected situation. By prioritizing user well-being,
+              GoldiBite ensures that individuals can handle emergencies with confidence and peace of mind.
             </p>
           </div>
           <div className="imageContent">
@@ -25,8 +25,6 @@ export default function Features() {
             />
           </div>
         </div>
-
-        {/* Usability */}
         <div className="valueCard reverseCard" style={{ margin: "0 10px" }}>
           <div className="imageContent">
             <Image
@@ -39,17 +37,17 @@ export default function Features() {
           <div className="textContent">
             <h3>Usability</h3>
             <p>
-              Many food safety apps struggle with user-friendliness, making it challenging for individuals to navigate them effectively. GoldiBite, however, is intentionally designed with simplicity and intuitiveness in mind, ensuring that users of all ages and technical skill levels can easily access and manage critical information about their food allergies and sensitivities.
+              Many food safety apps struggle with user-friendliness, making it challenging for individuals to navigate them effectively. GoldiBite, however,
+              is intentionally designed with simplicity and intuitiveness in mind, ensuring that users of all ages and technical skill levels can easily access and manage critical information about their food allergies and sensitivities.
             </p>
           </div>
         </div>
-
-        {/* Accessible Features */}
         <div className="valueCard" style={{ margin: "0 10px" }}>
           <div className="textContent">
             <h3>Accessible to All Features</h3>
             <p>
-              GoldiBite provides users with comprehensive tools and reliable information to make informed and safe food choices, especially in situations like dining out or exploring unfamiliar environments. By offering valuable insights and support, the app enables users to navigate their food options with ease and confidence. With GoldiBite, users can travel and dine freely, assured that their safety and well-being are always a priority.
+              GoldiBite provides users with comprehensive tools and reliable information to make informed and safe food choices, especially in situations like dining out or exploring unfamiliar environments. By offering valuable insights and support, the app enables users to navigate their food options with ease and confidence. With GoldiBite, users can travel and dine freely,
+              assured that their safety and well-being are always a priority.
             </p>
           </div>
           <div className="imageContent">
@@ -61,13 +59,11 @@ export default function Features() {
             />
           </div>
         </div>
-
-        {/* Global Portability */}
         <div className="valueCard reverseCard" style={{ margin: "0 10px" }}>
           <div className="imageContent">
             <Image
               src="/images/global.jpg"
-              alt="Feature 4"
+              alt="Feature 3"
               width={150}
               height={150}
             />
@@ -75,7 +71,8 @@ export default function Features() {
           <div className="textContent">
             <h3>Global Portability</h3>
             <p>
-              GoldiBite’s features are thoughtfully designed to be functional and valuable across a wide range of countries. The app provides localized information tailored to the user’s specific location, including critical details such as emergency hotlines, local allergen regulations, and region-specific guidance. This ensures users have access to the support and information they need, no matter where they are in the world.
+              GoldiBite’s features are thoughtfully designed to be functional and valuable across a wide range of countries. The app provides localized information tailored to the user’s specific location, including critical details such as emergency hotlines, local allergen regulations, and region-specific guidance. This ensures users have access to the support and information they need, 
+              no matter where they are in the world.
             </p>
           </div>
         </div>
